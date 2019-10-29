fe0b8fb5d010c1db9d11113aa6909920118cee0c
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-10-29 16:11:52 +0100

    Add project files.

---
+++ PASSCloudMobileBot/PASSCloudMobileBot/Helpers/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace PASSCloudMobileBot.Helpers
+{
+    public static class Constants
+    {
+        public readonly static string WebChatKey = "your-web-chat-key";
+        public readonly static string TokenName = "AdventureWorksBotToken3";
+    }
+}
---
+++ PASSCloudMobileBot/PASSCloudMobileBot/Views/BotWebViewPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PASSCloudMobileBot.Helpers;
+using PASSCloudMobileBot.Services;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PASSCloudMobileBot.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BotWebViewPage : ContentPage
+    {
+        public BotWebViewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var tokenExists = Application.Current.Properties.ContainsKey(Constants.TokenName);
+            string token;
+
+            if (!tokenExists)
+            {
+                token = await BotService.GetToken();
+                Application.Current.Properties[Constants.TokenName] = token;
+            }
+            else
+                token = Application.Current.Properties[Constants.TokenName].ToString();
+
+            string botWebViewUrl = "https://webchat.botframework.com/embed/AdventureWorksBotGAB19Munich?s=" + token;
+            BotWebView.Source = botWebViewUrl;
+        }
+    }
+}
